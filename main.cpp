cbb9c1a9c212db1e81257782a7f200ce72204eaf
Quinn Huettner <quinn1997@charter.net>
2018-11-15 09:56:42 -0600

    process are created

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include "process.hpp"
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    ofstream file;
+    ifstream file;
     file.open (argv[1]);
+    string line;
 
-    while(file.is_open()){
+   if (file.is_open()) {
+    Process* p = new Process();
 
-
-    }
+    while (file >> p->pid >> p->arr >> p->burst >> p->pri >> p->deadline >> p->io) {
+       p->print();
+           }
+    file.close();
+  }
 
 
 
