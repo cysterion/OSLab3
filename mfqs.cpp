d0e95bc07d77aead0e87daa80a47e708b6109f0b
whoisronnoc <connor@ronnoc.info>
2018-12-01 16:57:51 -0600

    read file

--- mfqs.cpp
+++ mfqs.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include "process.hpp"
 #include <queue>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
 
     //prompt for queues and tq
 
     //set up queues
-		std::priority_queue<Process, std::vector<Process>, std::greater<Process>> readyQueue;
-    Process* p = new Process(1,2,3,4,5,6);
-		Process* p2 = new Process(2,3,2,5,6,7);
+	std::priority_queue<Process, std::vector<Process>, std::greater<Process>> readyQueue;
+//    Process* p = new Process(1,2,3,4,5,6);
+//		Process* p2 = new Process(2,3,2,5,6,7);
+//    readyQueue.push(*p);
 	
-    readyQueue.push(*p);
-		readyQueue.push(*p2);
 	
-		Process p3 = readyQueue.top();
-		p3.print();
+	ifstream file;
+//	cout << argv[1];
+	file.open (argv[1]);
+	string line;
+	
+	if (file.is_open()) {
+		Process* p = new Process();
+		
+		while (file >> p->pid >> p->arr >> p->burst >> p->pri >> p->deadline >> p->io) {
+//			p->print();
+			readyQueue.push(*p);
+		}
+		file.close();
+	}
+	
+	Process t = readyQueue.top();
+	t.print();
 	
     //read in file and create processes
 
