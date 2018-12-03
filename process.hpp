0326fb975a435bd867917997791b96e5cb20c836
whoisronnoc <connor@ronnoc.info>
2018-12-02 20:15:00 -0600

    real time running 🏃‍♂️

--- process.hpp
+++ process.hpp
@@ -16,10 +16,10 @@ class Process{
     Process();
     Process(int pid, int bst, int arr, int pri, int ddline, int io);
     ~Process();
-//
-//		friend bool operator<(const Process& a, const Process& b) {
-//			return dline > a.arr;
-//		}
+
+		friend bool operator<(const Process& a, const Process& b) {
+			return a.deadline > b.deadline;
+		}
 		friend bool operator>(const Process& a, const Process& b) {
 			return b.arr < a.arr;
 		}
--- rts.cpp
+++ rts.cpp
@@ -16,6 +16,8 @@ int main(int argc, char* argv[]) {
 	
 	priority_queue<Process, vector<Process>, greater<Process>> readyQueue;
 	
+	priority_queue<Process, vector<Process>, less<Process>> queue;
+	
 	//read in file and create processes
 	ifstream file;
 	//	cout << argv[1];
@@ -34,19 +36,15 @@ int main(int argc, char* argv[]) {
 	
 	//do the scheduler
 	unsigned int tick = 0;
-	int runtime = 0;
-	bool running = false;
 	Process runningProcess;
-	bool processessLeft = true;
 	
-	while(!readyQueue.empty() || processessLeft) {
+	while(!readyQueue.empty() || !queue.empty()) {
 		
 		Process p = readyQueue.top();
-		
 		while (p.arr == tick && !readyQueue.empty()){
-			cout << "Added: " << p.pid << " to Q: 0" << endl;
+			cout << "Added: " << p.pid << " to Q" << endl;
 			
-			queues[0].push_back(p);
+			queue.push(p);
 			readyQueue.pop();
 			
 			if (!readyQueue.empty()) {
@@ -54,100 +52,29 @@ int main(int argc, char* argv[]) {
 			}
 		}
 		
-		if(running) {
-			runningProcess.burst -= 1;
-			runtime++;
-			cout << "Currently Running: " << runningProcess.pid << endl;
+		if (!queue.empty()) {
+			runningProcess = queue.top();
+			queue.pop();
 			
-			if(runningProcess.burst == 0) {
-				running = false;
-				runtime = 0;
-				cout << "Process: " << runningProcess.pid << " finished" << endl;
+			runningProcess.burst -= 1;
+			cout << "Process " << runningProcess.pid << " is running 🏃‍♂️" << endl;
+			
+			if (runningProcess.deadline <= tick) {
+				cout << "Process " << runningProcess.pid << " failed to meet deadline" << endl;
+				
+				if(type == 0){
+					exit(-1);
+				}
 				
 			} else {
-				if((runtime >= timeQuantum) && (sourceQ < (numQ-1))) {
-					cout << "Demoted: " << runningProcess.pid << " to Q: " << sourceQ+1 << endl;
-					queues[sourceQ+1].push_back(runningProcess);
-					runtime = 0;
-					running = false;
-				}
-				
-				if ((runtime >= timeQuantum) && (sourceQ == (numQ-1))) {
-					queues[numQ-1].push_back(runningProcess);
-					runtime = 0;
-					running = false;
-				}
-			}
-			
-		} else {
-			int i = 0;
-			deque<Process> currQ = queues[i];
-			while(currQ.empty() && i < numQ) {
-				currQ = queues[++i];
-			}
-			
-			if (i < numQ) {
-				runningProcess = queues[i].front();
-				queues[i].pop_front();
-				
-				sourceQ = i;
-				if(sourceQ == (numQ-1)) {
-					timeQuantum = 1;
-				} else {
-					timeQuantum = tq * pow(2, i);
-				}
-				running = true;
-				
-				//				runtime++;
-				//				runningProcess.burst -= 1;
-				//				cout << "Currently Running: " << runningProcess.pid << endl;
-			}
-		}
-		
-		//		if (running) {
-		//			cout << "Currently Running: " << runningProcess.pid << endl;
-		//		}
-		//		cout << tick << endl;
-		if (readyQueue.empty()) {
-			
-			for(int i = 0; i < numQ; i++) {
-				if (!queues[i].empty()) {
-					processessLeft = true;
-					break;
+				if (runningProcess.burst == 0) {
+					cout << "Process " << runningProcess.pid << " completed" << endl;
 					
 				} else {
-					if (runningProcess.burst == 0) {
-						processessLeft = false;
-					}
+					queue.push(runningProcess);
 				}
 			}
-			
 		}
-		
-		//		deque<Process> aq = queues[numQ-1];
-		//		for (int i = 0; i < aq.size(); i++) {
-		//			aq[i].age += 1;
-		
-		//			if (aq[i].age >= aInt) {
-		//				cout << "Aged up: " << aq[i].pid << endl;
-		//				aq[i].age = 0;
-		//				queues[numQ-2].push_back(aq[i]);
-		//				queues[numQ-1].erase(aq.begin()+i);
-		//			}
-		
-		//		}
-		
-		//		for (auto it = queues[numQ-1].begin(); it != queues[numQ-1].end(); ++it) {
-		//			cout << *it;
-		//			it->age += 1;j
-		//			if (it->age >= aInt) {
-		//				cout << "Aged up: " << it->pid << endl;
-		////				it->age = 0;
-		//				queues[numQ-2].push_back(*it);
-		//				queues[numQ-1].erase(it);
-		//			}
-		//		}
-		
 		
 		tick++;
 	}
